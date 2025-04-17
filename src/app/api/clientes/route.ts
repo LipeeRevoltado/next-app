1f4bc647fc5017ff8ee1075850151ab534185e29
Felipe <felipe@fortalezatec.com.br>
2025-04-17 16:35:27 -0300

    Ajuste no código para atualizar o Deploy pelo Vercel

--- src/app/api/clientes/route.ts
+++ src/app/api/clientes/route.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from '../../../../lib/db';
 
-export async function PUT(request: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+type Context = {
+  params: { id: string };
+};
+
+export async function PUT(request: NextRequest, { params }: Context) {
   const { nome, telefone } = await request.json();
-  await pool.query('UPDATE cliente SET nome = ?, telefone = ? WHERE id = ?', [nome, telefone, id]);
+  await pool.query('UPDATE cliente SET nome = ?, telefone = ? WHERE id = ?', [nome, telefone, params.id]);
   return NextResponse.json({ message: 'Cliente atualizado' });
 }
 
-export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
-  await pool.query('DELETE FROM cliente WHERE id = ?', [id]);
+export async function DELETE(request: NextRequest, { params }: Context) {
+  await pool.query('DELETE FROM cliente WHERE id = ?', [params.id]);
   return NextResponse.json({ message: 'Cliente deletado' });
 }
