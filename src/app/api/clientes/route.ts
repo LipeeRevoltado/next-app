db073cb5b11f58b8f0c4436038e9c06b6b9e6704
Felipe <felipe@fortalezatec.com.br>
2025-04-17 16:32:43 -0300

    Ajuste para Deploy no Vercel

--- src/app/api/clientes/route.ts
+++ src/app/api/clientes/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import pool from '../../../../lib/db';
 
-export async function PUT(request: Request, context: { params: { id: string } }) {
+export async function PUT(request: NextRequest, context: { params: { id: string } }) {
   const { id } = context.params;
   const { nome, telefone } = await request.json();
   await pool.query('UPDATE cliente SET nome = ?, telefone = ? WHERE id = ?', [nome, telefone, id]);
   return NextResponse.json({ message: 'Cliente atualizado' });
 }
 
-export async function DELETE(_: Request, context: { params: { id: string } }) {
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
   const { id } = context.params;
   await pool.query('DELETE FROM cliente WHERE id = ?', [id]);
   return NextResponse.json({ message: 'Cliente deletado' });
