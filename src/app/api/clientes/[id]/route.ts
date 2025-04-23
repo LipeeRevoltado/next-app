b89a66c52dbbac59837bbd56367aec3dd4e210c6
Felipe <felipe@fortalezatec.com.br>
2025-04-23 11:08:35 -0300

    fix - PUT clientes

--- src/app/api/clientes/[id]/route.ts
+++ src/app/api/clientes/[id]/route.ts
@@ -1,15 +1,25 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import pool from '../../../../../lib/db';
 
-export async function PUT(request: Request, context: { params: { id: string } }) {
-  const { params } = context;
+type Props = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+export async function PUT(request: NextRequest, props: Props) {
+  const params = await props.params
+
+  console.log('params', params);
+
   const { nome, telefone } = await request.json();
   await pool.query('UPDATE cliente SET nome = ?, telefone = ? WHERE id = ?', [nome, telefone, params.id]);
   return NextResponse.json({ message: 'Cliente atualizado' });
 }
 
-export async function DELETE(_: Request,  context: { params: { id: string } }) {
-  const { params } = context;
+export async function DELETE(_: Request,  props: Props) {
+  const params = await props.params
+
   await pool.query('DELETE FROM cliente WHERE id = ?', [params.id]);
   return NextResponse.json({ message: 'Cliente deletado' });
 }
